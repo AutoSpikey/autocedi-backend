a967821c984c1d79bd9535a4009fa847be1f621c
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-11-22 10:46:25 +0000

    register automationRoutes

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose');
+const automationRoutes = require('./automation.routes');
+const audit = require("express-requests-logger")
+const cors = require('cors')
+
 const app = express();
 const PORT = 3000
+
+app.use(express.json());
+app.use(audit())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the autocedi backend');
 });
 
-app.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
-})
+// Use the automation routes
+app.use('/automations', automationRoutes);
+
+// Connect to MongoDB
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.p5etw.mongodb.net/your_database?retryWrites=true&w=majority`)
+    .then(res => {
+        app.listen(PORT, () => {
+            console.log('listening on port ' + PORT);
+        })
+    }).catch(err => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
