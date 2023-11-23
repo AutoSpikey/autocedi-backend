e06a0e2ecbd584dffd9f021f65b2e53a8f2a22c3
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-11-23 18:04:28 +0000

    setup express to deploy to vercel

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const automationRoutes = require('./automation.routes');
+const callbackRoutes = require('./callback.routes');
+
+const audit = require("express-requests-logger")
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json());
+app.use(audit())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the autocedi backend');
+});
+
+// Use the automation routes
+app.use('/automations', automationRoutes);
+app.use('/callback', callbackRoutes);
+
+module.exports = app;
+
+
--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-require('dotenv').config()
-const express = require('express')
+const app = require("./app");
 const mongoose = require('mongoose');
-const automationRoutes = require('./automation.routes');
-const callbackRoutes = require('./callback.routes');
-
-const audit = require("express-requests-logger")
-const cors = require('cors')
-
-const app = express();
 const PORT = 3000
-
-app.use(express.json());
-app.use(audit())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the autocedi backend');
-});
-
-// Use the automation routes
-app.use('/automations', automationRoutes);
-app.use('/callback', callbackRoutes);
-
 
 // Connect to MongoDB
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.p5etw.mongodb.net/your_database?retryWrites=true&w=majority`)
