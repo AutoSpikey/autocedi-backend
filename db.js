9e3c066026378828dcd148af961e9fe12ffe90e6
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-11-28 11:05:21 +0000

    fix database connection

--- db.js
+++ db.js
@@ -5,4 +5,4 @@ async function connect() {
     await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.p5etw.mongodb.net/autocedi?retryWrites=true&w=majority`)
 }
 
-module.exports = connect
+module.exports = { connect }
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const app = require("./app");
-const mongoose = require('mongoose');
+const db = require("./db");
 const PORT = 3000
 
 // Connect to MongoDB
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.p5etw.mongodb.net/your_database?retryWrites=true&w=majority`)
-    .then(res => {
-        app.listen(PORT, () => {
-            console.log('listening on port ' + PORT);
-        })
-    }).catch(err => {
+async function main(){
+    await db.connect().catch(err => {
         console.error('Error connecting to MongoDB:', err.message);
     });
+
+    app.listen(PORT, () => {
+        console.log('listening on port ' + PORT);
+    })
+}
+
+main();
