3e40c3141d5206e36af7d492ab3801d05bf7d0aa
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-11-28 11:07:11 +0000

    implement register

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const router = express.Router(); router.post('/', async (req, res) => {
-    console.log(req.body)
-    return res.json({
-        "message": "not implemented yet"
-    }, 200)
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require("../models/user.model")
+
+// Create
+router.post('/', async (req, res) => {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+
+    try {
+        const user = await User.create(req.body);
+        return res.json(user);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 });
 
 module.exports = router;
