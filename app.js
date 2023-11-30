370e8b893a0fa763272876692f7ed71f50a478db
kkopoku <kk.opoku@outlook.com>
2023-11-30 11:38:03 +0000

    updated express-rate-limit window period

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const validateRoutes = require("./routes/validate.routes");
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 60 * 1000, // 1 minute
   max: 100, // Limit each IP to 100 requests per window
 });
 
