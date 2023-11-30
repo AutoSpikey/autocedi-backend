1f142229cfca23884d87e58718dc491bccf79672
kkopoku <kk.opoku@outlook.com>
2023-11-30 10:30:55 +0000

    added rate limiting

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-const express = require('express');
+const express = require("express");
 
-const automationRoutes = require('./routes/automation.routes');
-const callbackRoutes = require('./routes/callback.routes');
-const loginRoutes = require('./routes/login.routes');
-const registerRoutes = require('./routes/register.routes');
-const walletRoutes = require('./routes/wallet.routes');
-const validateRoutes = require('./routes/validate.routes');
+const automationRoutes = require("./routes/automation.routes");
+const callbackRoutes = require("./routes/callback.routes");
+const loginRoutes = require("./routes/login.routes");
+const registerRoutes = require("./routes/register.routes");
+const walletRoutes = require("./routes/wallet.routes");
+const validateRoutes = require("./routes/validate.routes");
+const rateLimit = require("express-rate-limit");
 
-const cors = require('cors')
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
+});
+
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 // app.use(audit())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 app.use(cors());
+app.use(limiter);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the autocedi backend');
+app.get("/", (req, res) => {
+  res.send("Welcome to the autocedi backend");
 });
 
 // Use the automation routes
-app.use('/automations', automationRoutes);
-app.use('/wallets', walletRoutes);
-app.use('/callback', callbackRoutes);
-app.use('/login', loginRoutes);
-app.use('/register', registerRoutes);
-app.use('/validate', validateRoutes);
+app.use("/automations", automationRoutes);
+app.use("/wallets", walletRoutes);
+app.use("/callback", callbackRoutes);
+app.use("/login", loginRoutes);
+app.use("/register", registerRoutes);
+app.use("/validate", validateRoutes);
 
 module.exports = app;
-
-
